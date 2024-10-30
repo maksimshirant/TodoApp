a096a26ae7110a3ece2f1dd13c973653b53c4bbd
Shirant Maksim <shirant.maksim@yandex.ru>
2024-10-30 21:01:08 +0300

    done

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import st from '../styles/toDoList.module.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
 import AllTasks from './AllTasks';
 import DoneTasks from './DoneTasks';
 import ActiveTasks from './ActiveTasks';
@@ -11,18 +11,30 @@ const ToDoList: FC = () => {
    return (
       <Router>
          <div style={{ display: 'flex', justifyContent: 'center', gap: '30px', marginBottom: '20px' }}>
-            <Link to="/alltasks">
-               <button className={st.linkButton}>Все задачи</button>
-            </Link>
-            <Link to="/donetasks">
-               <button className={st.linkButton}>Завершенные</button>
-            </Link>
-            <Link to="/activetasks">
-               <button className={st.linkButton}>Активные</button>
-            </Link>
-            <Link to="/trash">
-               <button className={st.linkButton}>Удаленные</button>
-            </Link>
+            <NavLink
+               to="/alltasks"
+               className={({ isActive }) => isActive ? `${st.linkButton} ${st.activeLink}` : st.linkButton}
+            >
+               Все задачи
+            </NavLink>
+            <NavLink
+               to="/donetasks"
+               className={({ isActive }) => isActive ? `${st.linkButton} ${st.activeLink}` : st.linkButton}
+            >
+               Завершенные
+            </NavLink>
+            <NavLink
+               to="/activetasks"
+               className={({ isActive }) => isActive ? `${st.linkButton} ${st.activeLink}` : st.linkButton}
+            >
+               Активные
+            </NavLink>
+            <NavLink
+               to="/trash"
+               className={({ isActive }) => isActive ? `${st.linkButton} ${st.activeLink}` : st.linkButton}
+            >
+               Удаленные
+            </NavLink>
          </div>
          <Routes>
             <Route path="/alltasks" element={<AllTasks />} />
